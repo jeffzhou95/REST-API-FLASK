ce0ec35ca6d94c99b4f56d2e100331352f464a0f
jeffzhou95 <xz6429@utexas.edu>
2018-12-16 21:05:44 -0600

    Implement Authorization using flask-jwt

--- code/app.py
+++ code/app.py
@@ -1,25 +1,53 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = "jeff"
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity) # /auth
 
 items = []
 
 class Item(Resource):
+	parser = reqparse.RequestParser()
+	parser.add_argument("price",
+		type = float,
+		required = True,
+		help = "This field cannot be left blank"
+	)
+
+	@jwt_required()
 	def get(self, name):
 		item = next(filter(lambda x: x["name"] == name, items), None)
-		return {"item": None}, 404 if item is None else 200
+		return {"item": item}, 404 if item is None else 200
 
 	def post(self, name):
 		if(next(filter(lambda x: x["name"] == name, items), None)):
 			return {"message": "An item with name '{}' already exists!".format(name)}, 400
 
-
-		data = request.get_json()
+		data = Item.parser.parse_args()
 		item = {"name": name, "price": data["price"]}
 		items.append(item)
 		return item, 201
+
+	def delete(self, name):
+		global items
+		items = list(filter(lambda x: x["name"] != name, items))
+		return {"message": "Item deleted"}
+
+	def put(self, name):
+		data = Item.parser.parse_args()
+
+		item = next(filter(lambda x: x["name"] == name, items), None)
+		if item is None:
+			item = {"name": name, "price": data["price"]}
+			items.append(item)
+		else:
+			item.update(data)
+		return item
 
 class ItemList(Resource):
 	def get(self):
