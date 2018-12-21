292ad5b0b9891ae83e3850df6929a375f5ba7ebf
jeffzhou95 <xz6429@utexas.edu>
2018-12-20 22:47:13 -0600

    Using SQLAlchemy instead of SQLite and add StoreModel class

--- s6/code/app.py
+++ s6/code/app.py
@@ -7,8 +7,14 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "jeff"
 api = Api(app)
+
+@app.before_first_request
+def create_tables():
+	db.create_all()
 
 jwt = JWT(app, authenticate, identity) # /auth
 
@@ -17,4 +23,6 @@ api.add_resource(Item, "/item/<string:name>")
 api.add_resource(UserRegister, "/register")
 
 if __name__ == "__main__":
+	from db import db
+	db.init_app(app)
 	app.run(port=5000, debug=True)
--- s6/code/resources/item.py
+++ s6/code/resources/item.py
@@ -1,4 +1,3 @@
-import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
@@ -9,6 +8,11 @@ class Item(Resource):
 		type = float,
 		required = True,
 		help = "This field cannot be left blank."
+	)
+	parser.add_argument("store_id",
+		type = int,
+		required = True,
+		help = "Every item needs a store id."
 	)
 
 	@jwt_required()
@@ -23,61 +27,38 @@ class Item(Resource):
 			return {"message": "An item with name '{}' already exists!".format(name)}, 400
 
 		data = Item.parser.parse_args()
-		item = ItemModel(name, data['price'])
+		item = ItemModel(name, **data)
 
 		try:
-			item.insert()
+			item.save_to_db()
 		except:
 			return {"message": "An error occurred when inserting item."}, 500 # Internal server error
 
 		return item.json(), 201
 
 	def delete(self, name):
-		if ItemModel.find_by_name(name) is None:
-			return {"message": "An item with name '{}' doesn't exist!".format(name)}, 404		
-
-		connection = sqlite3.connect("data.db")
-		cursor = connection.cursor()
-
-		query = "DELETE FROM items WHERE name=?"
-		cursor.execute(query, (name,))
-
-		connection.commit()
-		connection.close()
-
-		return {"message": "Item deleted."}
+		item = ItemModel.find_by_name(name)
+		if item:
+			item.delete_from_db()
+			return {"message": "Item deleted."}
+		else:
+			return {"message": "Item not found."}
 
 	def put(self, name):
 		data = Item.parser.parse_args()
 
 		item = ItemModel.find_by_name(name)
-		update_item = ItemModel(name, data['price'])
 
 		if item is None:
-			try:
-				update_item.insert()
-			except:
-				return {"message": "An error occurred when inserting item."}, 500				
+			item = ItemModel(name, **data)				
 		else:
-			try:
-				update_item.update()
-			except:
-				return {"message": "An error occurred when updating item."}, 500	
-		return update_item.json()
+			item.price = data['price']
+
+		item.save_to_db()
+
+		return item.json()
 	
 
 class ItemList(Resource):
 	def get(self):
-		connection = sqlite3.connect("data.db")
-		cursor = connection.cursor()
-
-		query = "SELECT * FROM items"
-		result = cursor.execute(query)
-		items = []
-		for row in result:
-			items.append({'name': row[0], 'price': row[1]})
-
-		connection.close()
-
-		return {'items': items}
-
+		return {'items': [item.json() for item in ItemModel.query.all()]} # All object in db
